e3bd7ed9f48cc10075621ed37d3cb8a01223ffbb
atul sonawale <atulsonawale@gmail.com>
2022-08-15 15:50:33 +0530

    spring-boot-security

---
+++ spring-boot-security/src/main/java/com/example/springbootsecurity/example1/HomeResource.java
@@ -0,0 +1,13 @@
+package com.example.springbootsecurity.example1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+	
+	@GetMapping("/")
+	public String home() {
+		return ("<h1>Welcome home</h1>");
+	}
+}
